46868182e0902b52813ed9b362579d0c536028d0
Lalfasa <147723708+Lalfasa@users.noreply.github.com>
2023-10-13 17:07:57 +0500

    llll.py

--- llll.py
+++ llll.py
@@ -1,11 +1,25 @@
 from pygame import *
-from time import time as timer
 
-back = (200, 255, 255)
+
+class GameSprite(sprite.Sprite):
+   def init(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       super().init()
+       # каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+       # каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+
+back = (200, 255, 255) 
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
-display.set_caption("пинг понг")
+display.set_caption("Пинг Понг")
 window.fill(back)
 
 game = True
